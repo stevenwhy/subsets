f2baff6f9db38adf0938868bfbf889df9bfde7b6
stevenwhy <stevenyildirim@gmail.com>
2022-03-31 17:36:21 -0400

    find subsets of list of ints

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,24 @@
 fun main() {
+
+    println("Here is the list of subsets: ${findSubsets(listOf(1,3))}")
+    println("Here is the list of subsets: ${findSubsets(listOf(1,5,3))}")
+}
+// O(2^n) time and space
+fun findSubsets(list: List<Int>): MutableList<List<Int>> {
+    val result = mutableListOf<List<Int>>(listOf())
+
+    var i = 0
+    while(i < list.size) {
+        val currentNumber = list[i]
+        var j = 0
+        val count = result.size
+        while(j < count) {
+            val newList = result[j].toMutableList()
+            newList.add(currentNumber)
+            result.add(newList)
+            j++
+        }
+        i++
+    }
+    return result
 }
