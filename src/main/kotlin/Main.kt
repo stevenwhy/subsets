b5535863da829802275079757ec21a2213191be4
stevenwhy <stevenyildirim@gmail.com>
2022-04-02 18:40:19 -0400

    find subsets of list of ints remove dupes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,9 @@ fun main() {
 
     println("Here is the list of subsets: ${findSubsets(listOf(1,3))}")
     println("Here is the list of subsets: ${findSubsets(listOf(1,5,3))}")
+
+    println("Here is the list of subsets no dupes: ${findSubsetsNoDupes(listOf(1,3,3))}")
+    println("Here is the list of subsets no dupes: ${findSubsetsNoDupes(listOf(1,5,3,3))}")
 }
 // O(2^n) time and space
 fun findSubsets(list: List<Int>): MutableList<List<Int>> {
@@ -18,6 +21,32 @@ fun findSubsets(list: List<Int>): MutableList<List<Int>> {
             result.add(newList)
             j++
         }
+        i++
+    }
+    return result
+}
+
+fun findSubsetsNoDupes(list: List<Int>): MutableList<List<Int>> {
+    val result = mutableListOf(listOf<Int>())
+    val sortedList = list.sorted()
+    var i = 0
+
+    var lastNumber = -1
+    var count = 0
+    while(i < sortedList.size) {
+        val currentNumber = sortedList[i]
+        var j = 0
+        if(currentNumber == lastNumber) {
+            j = count
+        }
+        count = result.size
+        while(j < count) {
+            val listToAdd = result[j].toMutableList()
+            listToAdd.add(currentNumber)
+            result.add(listToAdd)
+            j++
+        }
+        lastNumber = currentNumber
         i++
     }
     return result
